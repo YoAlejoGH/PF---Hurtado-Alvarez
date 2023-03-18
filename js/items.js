0345b5cab398b733318c90656731b74f5c3c4cec
YoAlejoGH <alejandrocolegio16@gmail.com>
2023-03-18 08:39:50 -0500

    Correcciones a estilo vizual

--- js/items.js
+++ js/items.js
@@ -22,4 +22,11 @@ const items = [
         img: "https://th.bing.com/th/id/R.7560a6023ec629015665170d62b81fbc?rik=FQ%2ffLk6tvXS0FA&pid=ImgRaw&r=0",
         cantidad: 1,
     },
+    {
+        id: 4,
+        nombre: "Apto Bogota",
+        precio: 320000,
+        img: "https://d2n37sn7igfnks.cloudfront.net/eyJidWNrZXQiOiJwYWRzLWltYWdlcyIsImtleSI6IjM5ZDFjMTg4LTU2MjgtNDFkYy05Zjk3LTQ2NDY3ZGY5NjU3ZC5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjIwMDAsImZpdCI6Imluc2lkZSJ9fX0=",
+        cantidad: 1,
+    },
 ];
